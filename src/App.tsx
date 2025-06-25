ba523c6b3de027f64fff01c7e136402f1b875204
jhose2906 <godsonjhose@gmail.com>
2025-06-26 00:34:31 +0530

    fix: margin

--- src/App.tsx
+++ src/App.tsx
@@ -92,7 +92,7 @@ export const App = () => {
           />
         </div>
 
-        <footer className=" text-center text-sm text-gray-500 dark:text-gray-400 mb-   ">
+        <footer className=" text-center text-sm text-gray-500 dark:text-gray-400 mb-3">
           <div className=" gap-2 flex items-center justify-center">
             <p>&copy; 2025 Jhose</p>
             <a
